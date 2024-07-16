d320540004387a114f0ddb0491b070e55121ab7b
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-07-16 12:44:34 -0700

    built Board and square components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
+function Square() {
+  const [value, setValue] = useState(null)
+  const handleClick = () => {
+    setValue('X')
+  }
+  return <button className="square" onClick={handleClick}>{value}</button>
+
+}
+
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="board-row">
+        <Square value={squares[0]}/>
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]}/>
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]}/>
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
+      </div>
+    </>
   );
 }
 
-export default App;
